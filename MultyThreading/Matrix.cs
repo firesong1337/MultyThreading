42d5036463b0b33b0df92eb9dc612682bcb4ec81
firesong1337 <kamil.iskhakov1337@gmail.com>
2023-04-03 22:32:40 +0500

    Добавьте файлы проекта.

---
+++ MultyThreading/Matrix.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultyThreading
+{
+    class Matrix
+    {
+        public int N { get; set; }
+        public int M { get; set; }
+        public int[,] A { get; set; }
+        public Matrix(int n, int m)
+        {
+            this.N = n;
+            this.M = m;
+            A = new int[n, m];
+        }
+        public void rand()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    A[i, j] = rnd.Next(1, 9);
+                    Console.Write("{0}\t", A[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        public List<int> SeekMin()
+        {
+            List<int> x = new List<int>(M);
+            for (int j = 0; j < M; j++)
+            {
+                int minHeight = A[0, j];
+                for (int i = 0; i < N; i++)
+                {
+                    if (A[i, j] < minHeight)
+                    {
+                        minHeight = A[i, j]; 
+                    }
+                }
+                x.Add(minHeight);
+                //Console.WriteLine("столбец {0}, значение: {1}", j, minHeight);
+            }
+            return x;
+        }
+
+    }
+}
---
+++ MultyThreading/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace MultyThreading
+{
+    class Program
+    {
+        public static int MultMin(List<int> lst)
+        {
+           return lst.Aggregate((x, y) => x * y);
+        }
+        static void Main(string[] args)
+        {
+            Matrix usingMx = new Matrix(4, 4);
+
+            usingMx.rand();
+            Console.WriteLine(String.Join(',',usingMx.SeekMin()));
+            Console.WriteLine(MultMin(usingMx.SeekMin()));
+
+        }
+    }
+}
+    
