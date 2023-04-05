7b9cc866f488943b56a3289ce31cfd6406a61995
Kamil <kamil.iskhakov1337@mail.ru>
2023-04-05 09:17:17 +0500

    Перенес PLINQ в метод

--- MultyThreading/Program.cs
+++ MultyThreading/Program.cs
@@ -8,6 +8,20 @@ namespace MultyThreading
 {
     class Program
     {
+        public static double CountThreads(int n, Matrix mx)
+        {
+            mx.rand();
+            //PLINQ
+            var query = from item in mx.SeekMin().AsParallel().WithDegreeOfParallelism(n)
+                        select MultMin(mx.SeekMin());
+            var sw = Stopwatch.StartNew();
+
+            // For pure query cost, enumerate and do nothing else.
+            foreach (var i in query) { }
+            sw.Stop();
+            double elapsed = sw.ElapsedMilliseconds;
+            return (elapsed/1000);
+        }
         public static int MultMin(List<int> lst)
         {
            return lst.Aggregate((x, y) => x * y);
@@ -19,23 +33,19 @@ namespace MultyThreading
             int Mas = Convert.ToInt32(val);
 
             Matrix usingMx = new Matrix(Mas, Mas);
-            usingMx.rand();
+            
 
-            //PLINQ
-            var query = from item in usingMx.SeekMin().AsParallel().WithDegreeOfParallelism(2)
-                        select MultMin(usingMx.SeekMin());
-            var sw = Stopwatch.StartNew();
+            double cnt = CountThreads(1, usingMx);
 
-            // For pure query cost, enumerate and do nothing else.
-            foreach (var n in query) { }
-            sw.Stop();
-            double elapsed = sw.ElapsedMilliseconds; // or sw.ElapsedTicks
-            Console.WriteLine("Total query time: {0} ms", elapsed);
+            Console.WriteLine("1 поток: \n Время выполнения {0}", cnt);
+            Console.WriteLine("=====================================");
+            Console.WriteLine("4 потокa: \n Время выполнения {0}\n Ускорение: {1} \n Эффективность: {2}", CountThreads(4, usingMx), cnt/CountThreads(4, usingMx), (cnt / CountThreads(4, usingMx)) / 4);
+            Console.WriteLine("=====================================");
+            Console.WriteLine("8 потокa: \n Время выполнения {0}\n Ускорение: {1} \n Эффективность: {2}", CountThreads(8, usingMx), cnt / CountThreads(8, usingMx), (cnt / CountThreads(8, usingMx)) / 8);
+            Console.WriteLine("=====================================");
+            Console.WriteLine("16 потокa: \n Время выполнения {0}\n Ускорение: {1} \n Эффективность: {2}", CountThreads(16, usingMx), cnt / CountThreads(16, usingMx), (cnt / CountThreads(16, usingMx)) / 16);
 
-            Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
-
-            //var query = usingMx.SeekMin().AsParallel().WithDegreeOfParallelism(2).Where(lst.Aggregate((x, y) => x * y)).Select(...)
         }
     }
 }
